b3f1900207599df3e4d4b27b8790f3c2fc16a161
JunyoungLim <jl3248@cornell.edu>
2020-10-02 21:55:15 +0900

    Add dgemm_mine and dgemm_mine2

--- dgemm_mine2.c
+++ dgemm_mine2.c
@@ -15,7 +15,7 @@ void square_dgemm(const int M,
     for (j = 0; j < M; ++j) {			
         for (k = 0; k < M; ++k){
 			for (i = 0; i < M; ++i) {
-                C[j*M+i  ] += A[k*M+i  ] * B_t[k*M+j];
+                C[j*M+i  ] += A[k*M+i  ] * B[j*M+k];
 			}
         }
     }
