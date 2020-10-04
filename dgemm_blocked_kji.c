588e717a8a7c630fed9770af0c845fc1e54142e2
JunyoungLim <jl3248@cornell.edu>
2020-10-05 08:57:53 +0900

    Modify loop order in blocked

--- dgemm_blocked_kji.c
+++ dgemm_blocked_kji.c
@@ -39,12 +39,12 @@ void square_dgemm(const int M, const double* restrict A, const double* restrict 
 {
     const int n_blocks = M / BLOCK_SIZE + (M%BLOCK_SIZE? 1 : 0);
     int bi, bj, bk;
-    for (bk = 0; bk < n_blocks; ++bk) {
-        const int k = bk * BLOCK_SIZE;
+    for (bi = 0; bi < n_blocks; ++bi) {
+        const int i = bi * BLOCK_SIZE;
         for (bj = 0; bj < n_blocks; ++bj) {
             const int j = bj * BLOCK_SIZE;
-            for (bi = 0; bi < n_blocks; ++bi) {
-                const int i = bi * BLOCK_SIZE;
+            for (bk = 0; bk < n_blocks; ++bk) {
+                const int k = bk * BLOCK_SIZE;
                 do_block(M, A, B, C, i, j, k);
             }
         }
