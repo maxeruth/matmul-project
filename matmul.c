0c28e05fc086bf6c5a22aaf207b680144ffcff72
Max R <mer335@cornell.edu>
2020-10-05 20:45:37 +0000

    Timing

--- matmul.c
+++ matmul.c
@@ -46,10 +46,10 @@ extern void square_dgemm();
   around some of the powers-of-two.
 */
 const int test_sizes[] = {
-    16, 32, 64, 128
+    16, 32, 64, 128,
 	// Adding 4,8,16
 	// 4, 8, 12, 16, 32, 64
-	// 128, 256, 512, 1024
+    256, 512, 1024
     //4, 8, 16, 31, 32, 96, 97, 127, 128, 129, 191, 192, 229,
 #if defined(DEBUG_RUN)
 # define MAX_SIZE 229u
