7e3757c1cc113d27e9d1f01a2a1c67a4bccbb2eb
Max R <mer335@cornell.edu>
2020-10-05 20:50:18 +0000

    Timing

--- dgemm_mine2.c
+++ dgemm_mine2.c
@@ -4,13 +4,7 @@ void square_dgemm(const int M,
                   const double* restrict A, const double* restrict B, double* restrict C)
 {
     int i, j, k;
-    double* restrict B_t = (double*) malloc(M * M * sizeof(double));
-    
-    for (i = 0; i < M; ++i) {
-        for (j = 0; j < M; ++j) {
-            B_t[i*M + j] = B[j*M + i];
-        }
-    }
+         
 
     for (j = 0; j < M; ++j) {			
         for (k = 0; k < M; ++k){
