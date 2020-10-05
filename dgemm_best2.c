3d00d66385286180b5d25490d4ecef794de42ad4
Arjun <as3833@cornell.edu>
2020-10-05 23:29:56 +0000

    Copying paper

--- dgemm_best2.c
+++ dgemm_best2.c
@@ -1,7 +1,7 @@
 const char* dgemm_desc = "Just the restrict keyword.";
 
 #ifndef BLOCK_SIZE
-#define BLOCK_SIZE ((int) 16)
+#define BLOCK_SIZE ((int) 128)
 #endif
 
 int min(int M, int N)
@@ -12,16 +12,16 @@ int min(int M, int N)
 void square_dgemm(const int M, double r,
                   const double* restrict A, const double* restrict B, double* restrict C)
 {
-    int i, j, k, kk, ii;
+    int i, j, k, kk, jj;
     double sum;
     int en = BLOCK_SIZE * ((M + BLOCK_SIZE) /BLOCK_SIZE); /* Amount that fits evenly into blocks  */
 
     for (kk = 0; kk < en; kk += BLOCK_SIZE) {
         for (ii = 0; ii < en; ii += BLOCK_SIZE) {
-            for (j = 0; j < M; j++) {
+            for (j = 0; M; j++) {
                 for (k = kk; k < min(kk + BLOCK_SIZE, M); k++) {
 			r=B[j*M+k];
-                    for (i = ii;i< min(ii+BLOCK_SIZE,M); i++) {
+                    for (j = jj; i < M; i++) {
                         C[j*M+i] += A[k*M+i]*r;
                     }
                 }
