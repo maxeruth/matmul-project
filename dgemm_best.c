2c5e721e2ef244b47a5fdb71c94a2cd60b12652d
Max R <mer335@cornell.edu>
2020-10-06 00:29:08 +0000

    Finally

--- dgemm_best.c
+++ dgemm_best.c
@@ -12,16 +12,16 @@ int min(int M, int N)
 void square_dgemm(const int M, double r,
                   const double* restrict A, const double* restrict B, double* restrict C)
 {
-    int i, j, k, kk, ii;
+    int i, j, k, kk, jj;
     double sum;
     int en = BLOCK_SIZE * ((M + BLOCK_SIZE) /BLOCK_SIZE); /* Amount that fits evenly into blocks  */
 
     for (kk = 0; kk < en; kk += BLOCK_SIZE) {
-        for (ii = 0; ii < en; ii += BLOCK_SIZE) {
-            for (j = 0; j < M; j++) {
+        for (jj = 0; jj < en; jj += BLOCK_SIZE) {
+            for (j = jj; j < min(jj+BLOCK_SIZE,M); j++) {
                 for (k = kk; k < min(kk + BLOCK_SIZE, M); k++) {
-			r=B[j*M+k];
-                    for (i = ii;i< min(ii+BLOCK_SIZE,M); i++) {
+		    r=B[j*M+k];
+                    for (i = 0;i<M; i++) {
                         C[j*M+i] += A[k*M+i]*r;
                     }
                 }
